1b63d6acc2b29326484be91e20642ad16aa03a67
Sebastian Burckhardt <sburckha@microsoft.com>
2020-11-09 06:49:14 -0800

    add partition parameter to eventhubs script

--- test/DurableTask.Netherite.Tests/create-eventhubs-instances.ps1
+++ test/DurableTask.Netherite.Tests/create-eventhubs-instances.ps1
@@ -2,15 +2,18 @@
 # From then on, these same instances are reused for all the runs.
 
 # Replace these to match your project
-
 $ResourceGroup = 'MyResourceGroup'
 $NameSpace = 'MyEventHubsNameSpace'
+
+# Number of partitions. This can NOT be changed after the taskhub is created,
+# so it limits the number of nodes to which Netherite scales out.
+$NumberPartitions = 12
 
 # This creates the necessary instances used by this architecture
 
 echo "Creating fresh EventHubs instances..."
 
-az eventhubs eventhub create --namespace-name $NameSpace --resource-group $ResourceGroup --name Partitions --message-retention 1 --partition-count 32
+az eventhubs eventhub create --namespace-name $NameSpace --resource-group $ResourceGroup --name Partitions --message-retention 1 --partition-count $NumberPartitions
 az eventhubs eventhub create --namespace-name $NameSpace --resource-group $ResourceGroup --name Clients0 --message-retention 1 --partition-count 32
 az eventhubs eventhub create --namespace-name $NameSpace --resource-group $ResourceGroup --name Clients1 --message-retention 1 --partition-count 32
 az eventhubs eventhub create --namespace-name $NameSpace --resource-group $ResourceGroup --name Clients2 --message-retention 1 --partition-count 32
