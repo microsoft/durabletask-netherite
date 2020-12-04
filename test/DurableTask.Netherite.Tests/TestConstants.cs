85ed10a890fb50ff08b1150587833faafe86777d
Sebastian Burckhardt <sburckha@microsoft.com>
2020-12-03 16:39:44 -0800

    specify defaults for connection strings if environment variables are not set

--- test/DurableTask.Netherite.Tests/TestConstants.cs
+++ test/DurableTask.Netherite.Tests/TestConstants.cs
@@ -9,6 +9,27 @@ namespace DurableTask.Netherite.Tests
 
     public static class TestConstants
     {
+        public static string GetEventHubsConnectionString()
+            // use the eventhubs connection string, if specified in environment, or else use the in-memory emulation with 4 partitions
+            => GetTestSetting("EventHubsConnectionString", "MemoryF:4");
+
+        public static string GetAzureStorageConnectionString()
+            // use the storage connection string, if specified in the environment, or else use the storage emulator
+            => GetTestSetting("StorageConnectionString", "UseDevelopmentStorage=true;");
+
+        public const string NetheriteTestPrefix = "NetheriteTest";
+
+        static string GetTestSetting(string name, string defaultValue)
+        {
+            var envSetting = Environment.GetEnvironmentVariable(NetheriteTestPrefix + name);
+            return envSetting ?? defaultValue;
+        }
+
+        public static string GetTestTaskHubName()
+        {
+            return "test-taskhub";
+        }
+
         public static NetheriteOrchestrationServiceSettings GetNetheriteOrchestrationServiceSettings()
         {
             return new NetheriteOrchestrationServiceSettings
@@ -37,22 +58,11 @@ namespace DurableTask.Netherite.Tests
         internal static TestOrchestrationHost GetTestOrchestrationHost(ILoggerFactory loggerFactory)
             => new TestOrchestrationHost(GetNetheriteOrchestrationServiceSettings(), loggerFactory);
 
-        public static string GetTestTaskHubName()
-        {
-            return "test-taskhub";
-        }
-
-        public const string DurableTaskTestPrefix = "DurableTaskTest";
 
         public static bool DeleteStorageBeforeRunningTests => true; // set to false for testing log-based recovery
 
-        public static string GetAzureStorageConnectionString() => GetTestSetting("StorageConnectionString", true);
-
         public static string GetStorageConnectionString()
         {
-            // NOTE: If using the local file system, modify GetEventHubsConnectionString use one of the memory options.
-            // return FasterStorage.LocalFileStorageConnectionString;
-
             return GetAzureStorageConnectionString();
         }
 
@@ -60,30 +70,6 @@ namespace DurableTask.Netherite.Tests
         {
             // we are using the same storage in the unit tests
             return GetAzureStorageConnectionString();
-        }
-
-        public static string GetEventHubsConnectionString()
-        {
-            // Memory means TransportChoices.Memory and StorageChoices.Memory
-            // return "Memory:1";
-            // return "Memory:4";
-            // return "Memory:32";
-
-            // MemoryF means TransportChoices.Memory and StorageChoices.Faster
-            // return "MemoryF:1";
-            // return "MemoryF:4";
-            // return "MemoryF:32";
-
-            // using an actual connection string means TransportChoices.EventHubs and StorageChoices.Faster
-            return GetTestSetting("EventHubsConnectionString", false);
-        }
-
-        static string GetTestSetting(string name, bool require)
-        {
-            var setting = Environment.GetEnvironmentVariable(DurableTaskTestPrefix + name);
-            return !string.IsNullOrEmpty(setting) || !require
-                ? setting
-                : throw new ArgumentNullException($"The environment variable {DurableTaskTestPrefix + name} must be defined for the tests to run");
         }
     }
 }
