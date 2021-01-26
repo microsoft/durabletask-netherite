11f82a2e3470a09a6dd1c0edd51b0fedeeeed8c7
Sebastian Burckhardt <sburckha@microsoft.com>
2021-01-25 16:30:00 -0800

    Add eventhubs consumer test (#16)
    * add a performance test for consuming EH events, and an event generator
    
    * reduce verbosity of tracing.

---
+++ test/EventConsumer/Event.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace EventConsumer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Newtonsoft.Json;
+
+    [JsonObject(MemberSerialization.OptOut)]
+    public struct Event
+    {
+        public int Partition { get; set; }
+
+        public long SeqNo { get; set; }
+
+        public byte[] Payload { get; set; }
+
+        public override string ToString()
+        {
+            return $"Event {this.Partition}.{this.SeqNo} size={this.Payload.Length}";
+        }
+    }
+}
