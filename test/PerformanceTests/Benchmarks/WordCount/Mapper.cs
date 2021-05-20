74d30fd54065b2b2b8afb9a229f312861bd5e1f0
Xiangfeng Zhu <xzhu0027@gmail.com>
2021-05-20 11:00:48 -0700

    clean up wordcount

--- test/PerformanceTests/Benchmarks/WordCount/Mapper.cs
+++ test/PerformanceTests/Benchmarks/WordCount/Mapper.cs
@@ -60,9 +60,8 @@ namespace PerformanceTests.WordCount
                         Stopwatch s = new Stopwatch();
                         s.Start();
 
-                        // setup connection to the blob storage
+                        // setup connection to the blob storage (change it to the Gutenberg storage connection string)
                         string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-                       
                         CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
                         CloudBlobClient serviceClient = cloudStorageAccount.CreateCloudBlobClient();
 
