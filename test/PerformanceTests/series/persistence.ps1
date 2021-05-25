b0c2f3cb9aff5335db83976f735a3fdef28320f3
Sebastian Burckhardt <sburckha@microsoft.com>
2021-05-25 13:48:32 -0700

    add series for persistence options

---
+++ test/PerformanceTests/series/persistence.ps1
@@ -0,0 +1,22 @@
+﻿#!/usr/bin/pwsh
+param (
+	$ResultsFile="./results.csv",
+	$PrintColumnNames=$true,
+	$tu=20
+)
+
+# read the settings that are common to all scripts
+. ./settings.ps1
+
+if ($PrintColumnNames)
+{
+	Add-Content -path $ResultsFile -value "plan,nodes,tag,test,tu,starttime,iteration,size,duration"
+}
+
+
+./series/runseries -Tag neth-12-ls -HubName L23 -Plan EP2 -NumNodes 8 -WaitForDeploy 80 -Orchestration BankTransaction -NumOrchestrations 1000 -PortionSize 0 -DelayAfterRun 50 -ResultsFile $ResultsFile -ThroughputUnits $tu
+
+
+#./series/runseries -Tag neth-12-ls -HubName L23 -Plan EP2 -NumNodes 8 -WaitForDeploy 80 -Orchestration HelloSequence5 -NumOrchestrations 10000 -PortionSize 200 -DelayAfterRun 50 -ResultsFile $ResultsFile -ThroughputUnits $tu
+#./series/runseries -Tag neth-12-ns -HubName N23 -Plan EP2 -NumNodes 8 -WaitForDeploy 80 -Orchestration HelloSequence5 -NumOrchestrations 10000 -PortionSize 200 -DelayAfterRun 50 -ResultsFile $ResultsFile -ThroughputUnits $tu
+#./series/runseries -Tag neth-12-gs -HubName G23 -Plan EP2 -NumNodes 8 -WaitForDeploy 80 -Orchestration HelloSequence5 -NumOrchestrations 10000 -PortionSize 200 -DelayAfterRun 50 -ResultsFile $ResultsFile -ThroughputUnits $tu
