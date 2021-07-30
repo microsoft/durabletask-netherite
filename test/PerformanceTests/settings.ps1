9b8dc8a7b660758d1e9eb2514785667f59830040
Sebastian Burckhardt <sburckha@microsoft.com>
2021-07-30 09:23:53 -0700

    undo overwrite of settings

--- test/PerformanceTests/settings.ps1
+++ test/PerformanceTests/settings.ps1
@@ -2,7 +2,7 @@
 Write-Host "Using parameters specified in settings.ps1."
 
 # always edit this parameter before running the scripts
-$name="functionssb1"
+$name="globally-unique-lowercase-alphanumeric-name-with-no-dashes"
 
 # review these parameters before running the scripts
 $location="westus2"
