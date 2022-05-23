86b0327da4d284d48303021e207f9a3fad2e7643
Sebastian Burckhardt <sburckha@microsoft.com>
2022-05-23 09:01:58 -0700

    simplify ScaleMonitor test project
    (cherry picked from commit ea536001db6c57b2f8b1160832859f886f614bff)

--- test/ScaleMonitor/Program.cs
+++ test/ScaleMonitor/Program.cs
@@ -15,62 +15,47 @@ namespace ScalingTests
     {
         static async Task Main(string[] args)
         {
-            var settings = new NetheriteOrchestrationServiceSettings()
-            {
-                HubName = "perftests",
-                PartitionManagement = PartitionManagementOptions.ClientOnly,
-                LogLevelLimit = LogLevel.Trace,
-                EventLogLevelLimit = LogLevel.Trace,
-                StorageLogLevelLimit = LogLevel.Trace,
-                TransportLogLevelLimit = LogLevel.Trace,
-                WorkItemLogLevelLimit = LogLevel.Trace,
-                ClientLogLevelLimit = LogLevel.Trace,
-                LoadMonitorLogLevelLimit = LogLevel.Trace,
-            };
-            settings.Validate((string connectionName) => Environment.GetEnvironmentVariable(connectionName));
-            var loggerFactory = new LoggerFactory();
-            loggerFactory.AddProvider(new ConsoleLoggerProvider());
-            var logger = loggerFactory.CreateLogger("Main");
 
-            logger.LogInformation("Starting OrchestrationService...");
-            var service = new NetheriteOrchestrationService(settings, loggerFactory);
-            var orchestrationService = (IOrchestrationService)service;
-            await orchestrationService.StartAsync();
+            ScalingMonitor scalingMonitor = new ScalingMonitor(
+                        Environment.GetEnvironmentVariable("AzureWebJobsStorage"),
+                        Environment.GetEnvironmentVariable("EventHubsConnection"),
+                        "DurableTaskPartitions",
+                         "perftests",
+                         (a, b, c) => Console.Out.WriteLine("Recommendation: {a} {b} {c}"),
+                         (a) => Console.Out.WriteLine("Information: {a}"),
+                         (a, b) => Console.Out.WriteLine("Error: {a} {b}"));
 
-            if (service.TryGetScalingMonitor(out ScalingMonitor scalingMonitor))
+            while (true)
             {
-                while (true)
+                Console.Out.WriteLine("Hit a number  to run scale decision for that workercount, or 'q' to exit");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                int workerCount;
+
+                if (keyInfo.KeyChar == 'q')
                 {
-                    logger.LogInformation("Hit a number  to run scale decision for that workercount, or 'q' to exit");
-                    ConsoleKeyInfo keyInfo = Console.ReadKey();
-                    int workerCount;
-
-                    if (keyInfo.KeyChar == 'q')
-                    {
-                        break;
-                    }
-                    else if (!int.TryParse($"{keyInfo.KeyChar}", out workerCount))
-                    {
-                        workerCount = 1;
-                    }
-
-                    logger.LogInformation("--------- Collecting Metrics...");
-                    var metrics = await scalingMonitor.CollectMetrics();
-                    logger.LogInformation(JsonConvert.SerializeObject(metrics, Formatting.Indented));
-                    logger.LogInformation($"--------- Making scale decision for worker count {workerCount}...");
-                    var decision = scalingMonitor.GetScaleRecommendation(workerCount, metrics);
-                    logger.LogInformation(JsonConvert.SerializeObject(decision, Formatting.Indented));
+                    break;
                 }
-            }
-            else
-            {
-                logger.LogError("failed to create scaling monitor.");
-            } 
+                else if (!int.TryParse($"{keyInfo.KeyChar}", out workerCount))
+                {
+                    workerCount = 1;
+                }
 
-            // shut down
-            logger.LogInformation("OchestrationService stopping...");
-            await orchestrationService.StopAsync();
-            logger.LogInformation("Done");
+                Console.Out.WriteLine("--------- Collecting Metrics...");
+                ScalingMonitor.Metrics metrics = default;
+                try
+                {
+                    metrics = await scalingMonitor.CollectMetrics();
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine($"Caught exception: {e}");
+                    continue;
+                }
+                Console.Out.WriteLine(JsonConvert.SerializeObject(metrics, Formatting.Indented));
+                Console.Out.WriteLine($"--------- Making scale decision for worker count {workerCount}...");
+                var decision = scalingMonitor.GetScaleRecommendation(workerCount, metrics);
+                Console.Out.WriteLine(JsonConvert.SerializeObject(decision, Formatting.Indented));
+            }
         }
     }
 }
