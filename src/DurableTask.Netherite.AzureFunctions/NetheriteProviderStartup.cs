6ba2552bb5ef10bcd460230989e33d44028e01a0
Surgupta <surgupta@microsoft.com>
2022-08-05 13:54:32 -0700

    Updating NetheriteProviderStartup

--- src/DurableTask.Netherite.AzureFunctions/NetheriteProviderStartup.cs
+++ src/DurableTask.Netherite.AzureFunctions/NetheriteProviderStartup.cs
@@ -1,18 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-
-using System.Collections.Concurrent;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Azure.WebJobs.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-
 // Reference: https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
-[assembly: WebJobsStartup(typeof(DurableTask.Netherite.AzureFunctions.NetheriteProviderStartup))]
+[assembly: Microsoft.Azure.WebJobs.Hosting.WebJobsStartup(typeof(DurableTask.Netherite.AzureFunctions.NetheriteProviderStartup))]
 
 namespace DurableTask.Netherite.AzureFunctions
 {
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+    using Microsoft.Azure.WebJobs.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
     class NetheriteProviderStartup : IWebJobsStartup
     {
         public void Configure(IWebJobsBuilder builder)
@@ -22,7 +19,6 @@ namespace DurableTask.Netherite.AzureFunctions
 #else
             builder.Services.AddSingleton<IDurabilityProviderFactory, NetheriteProviderPseudoFactory>();
 #endif
-            builder.AddDurableTask();
         }
     }
 }
