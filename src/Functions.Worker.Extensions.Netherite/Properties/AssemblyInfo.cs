0c1c8abdeae838673a0499376b41e0e38201ecbe
Jacob Viau <javia@microsoft.com>
2023-01-05 10:32:34 -0800

    Add Functions isolated extension package

---
+++ src/Functions.Worker.Extensions.Netherite/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+// This must be updated when updating the version of the package
+[assembly: ExtensionInformation("Microsoft.Azure.DurableTask.Netherite.AzureFunctions", "1.3.0")]
