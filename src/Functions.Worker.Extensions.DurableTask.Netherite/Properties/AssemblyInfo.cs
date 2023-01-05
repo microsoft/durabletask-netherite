45831eeaebbb7e3770b9d432e157209bf02adafd
Jacob Viau <javia@microsoft.com>
2023-01-05 10:53:54 -0800

    Update copyright header

--- src/Functions.Worker.Extensions.DurableTask.Netherite/Properties/AssemblyInfo.cs
+++ src/Functions.Worker.Extensions.DurableTask.Netherite/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
-
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
