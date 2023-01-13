b675ec383f3ef4c8ebf5bd842b8a854fc4a34180
Jacob Viau <javia@microsoft.com>
2023-01-12 16:24:30 -0800

    Add Functions isolated extension package (#212)

---
+++ src/Functions.Worker.Extensions.DurableTask.Netherite/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+// This must be updated when updating the version of the package
+[assembly: ExtensionInformation("Microsoft.Azure.DurableTask.Netherite.AzureFunctions", "1.3.0")]
