b761ccb17a71ad025e6c551c9a0b11df4f76a3de
sebastianburckhardt <sburckha@microsoft.com>
2023-03-09 14:28:48 -0800

    fix versions for isolated worker

--- src/Functions.Worker.Extensions.DurableTask.Netherite/Properties/AssemblyInfo.cs
+++ src/Functions.Worker.Extensions.DurableTask.Netherite/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 
 // This must be updated when updating the version of the package
-[assembly: ExtensionInformation("Microsoft.Azure.DurableTask.Netherite.AzureFunctions", "1.3.0", true)]
+[assembly: ExtensionInformation("Microsoft.Azure.DurableTask.Netherite.AzureFunctions", "1.3.2", true)]
