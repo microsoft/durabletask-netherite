878b70fca2c5b8b81303204be28dd8d10709a0c9
David Justo <david.justo.1996@gmail.com>
2023-08-28 12:55:01 -0700

    removed duplicate 1.1.1 for 1.1.2

--- src/DurableTask.Netherite/StorageLayer/Faster/StoreWorker.cs
+++ src/DurableTask.Netherite/StorageLayer/Faster/StoreWorker.cs
@@ -439,7 +439,7 @@ namespace DurableTask.Netherite.Faster
                         (this.lastCheckpointedCommitLogPosition, this.lastCheckpointedInputQueuePosition)
                            = await this.pendingStoreCheckpoint; // observe exceptions here
 
-                        this.traceHelper.FasterProgress("SM Debug: state 1.1.1 - this.pendingStoreCheckpoint.IsCompleted, returned from pendingStoreCheckpoint");
+                        this.traceHelper.FasterProgress("SM Debug: state 1.1.2 - this.pendingStoreCheckpoint.IsCompleted, returned from pendingStoreCheckpoint");
 
                         // force collection of memory used during checkpointing
                         GC.Collect();
