37f49adde5d56cdafb665743667ef18d84b241e3
Sebastian Burckhardt <sburckha@microsoft.com>
2024-03-15 12:57:01 -0700

    Add RecoveryTester feature (#341)
    * refactor partition management options, remove Scripted partition manager, and add RecoveryTester feature
    
    * prevent creation of fresh taskhub in partition recovery mode

---
+++ src/DurableTask.Netherite/TransportLayer/EventHubs/IPartitionManager.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace DurableTask.Netherite.EventHubsTransport
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// General interface for starting and stopping the logic that activates the partition and load monitor on hosts.
+    /// </summary>
+    interface IPartitionManager
+    {
+        Task StartHostingAsync();
+
+        Task StopHostingAsync();
+    }
+}
