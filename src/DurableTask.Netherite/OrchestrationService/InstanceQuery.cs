24efd5478d9982fdbcc93e4ab27470910733c9a9
Sebastian Burckhardt <sburckha@microsoft.com>
2024-04-26 09:31:15 -0700

    Add support for isolated entities (#358)

--- src/DurableTask.Netherite/OrchestrationService/InstanceQuery.cs
+++ src/DurableTask.Netherite/OrchestrationService/InstanceQuery.cs
@@ -52,6 +52,11 @@ namespace DurableTask.Netherite
         [DataMember]
         internal bool PrefetchHistory { get; set; }
 
+        /// <summary>
+        /// Whether to exclude entities from the results.
+        /// </summary>
+        [DataMember]
+        internal bool ExcludeEntities { get; set; }
 
         /// <summary>
         /// Construct an instance query with the given parameters.
@@ -77,9 +82,6 @@ namespace DurableTask.Netherite
 
         internal bool HasRuntimeStatus => this.RuntimeStatus != null && this.RuntimeStatus.Length > 0;
 
-        internal bool IsSet => this.HasRuntimeStatus || !string.IsNullOrWhiteSpace(this.InstanceIdPrefix)
-                                    || !(this.CreatedTimeFrom is null) || !(this.CreatedTimeTo is null);
-
         internal bool Matches(OrchestrationState targetState)
         {
             if (targetState == null)
@@ -88,7 +90,8 @@ namespace DurableTask.Netherite
             return (!this.HasRuntimeStatus || this.RuntimeStatus.Contains(targetState.OrchestrationStatus))
                      && (string.IsNullOrWhiteSpace(this.InstanceIdPrefix) || targetState.OrchestrationInstance.InstanceId.StartsWith(this.InstanceIdPrefix))
                      && (!this.CreatedTimeFrom.HasValue || targetState.CreatedTime >= this.CreatedTimeFrom.Value)
-                     && (!this.CreatedTimeTo.HasValue || targetState.CreatedTime <= this.CreatedTimeTo.Value);
+                     && (!this.CreatedTimeTo.HasValue || targetState.CreatedTime <= this.CreatedTimeTo.Value)
+                     && (!this.ExcludeEntities || !DurableTask.Core.Common.Entities.IsEntityInstance(targetState.OrchestrationInstance.InstanceId));
         }
     }
 }
